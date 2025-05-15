a0939f59b5673be472be33359215ed8ecb5b1bee
kyxh1095 <759177847@qq.com>
2025-05-15 21:33:15 +0800

    Update train.py

--- train.py
+++ train.py
@@ -2,5 +2,5 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     model = YOLO('ultralytics/cfg/MFEL-YOLO/MFEL-YOLO.yaml')
-    model = model.load("yolov8s.pt")
+    #model = model.load("yolov8s.pt")
     model.train(data='ultralytics/cfg/datasets/AITOD.yaml',batch=8,epochs=70,imgsz=640)
